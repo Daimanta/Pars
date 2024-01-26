cee2c558e443b12c82b0a940ee56e8633c47f45b
Léon van der Kaap <leonkaap@gmail.com>
2024-01-27 00:22:11 +0100

    Formatted. Started on watch.

--- src/main.zig
+++ src/main.zig
@@ -8,12 +8,36 @@ const version = @import("version.zig");
 
 const default_allocator = std.heap.page_allocator;
 
-const Mode = enum {
-    check,
-    info,
-    create,
-    watch
-};
+const Mode = enum { check, info, create, watch };
+
+const IN_ACCESS = 0x00000001;
+const IN_MODIFY = 0x00000002;
+const IN_ATTRIB = 0x00000004;
+const IN_CLOSE_WRITE = 0x00000008;
+const IN_CLOSE_NOWRITE = 0x00000010;
+const IN_CLOSE = (IN_CLOSE_WRITE | IN_CLOSE_NOWRITE);
+const IN_OPEN = 0x00000020;
+const IN_MOVED_FROM = 0x00000040;
+const IN_MOVED_TO = 0x00000080;
+const IN_MOVE = (IN_MOVED_FROM | IN_MOVED_TO);
+const IN_CREATE = 0x00000100;
+const IN_DELETE = 0x00000200;
+const IN_DELETE_SELF = 0x00000400;
+const IN_MOVE_SELF = 0x00000800;
+const IN_ALL_EVENTS = 0x00000fff;
+
+const IN_UNMOUNT = 0x00002000;
+const IN_Q_OVERFLOW = 0x00004000;
+const IN_IGNORED = 0x00008000;
+
+const IN_ONLYDIR = 0x01000000;
+const IN_DONT_FOLLOW = 0x02000000;
+const IN_EXCL_UNLINK = 0x04000000;
+const IN_MASK_CREATE = 0x10000000;
+const IN_MASK_ADD = 0x20000000;
+
+const IN_ISDIR = 0x40000000;
+const IN_ONESHOT = 0x80000000;
 
 const params = [_]clap.Param(clap.Help){
     clap.parseParam("-c, --check <FILE>     Checks a parity file for data file consistency. Cannot be combined with -imw.") catch unreachable,
@@ -29,7 +53,6 @@ const params = [_]clap.Param(clap.Help){
 };
 
 pub fn main() !void {
-
     var iter = try clap.args.OsIterator.init(default_allocator);
     defer iter.deinit();
 
@@ -60,7 +83,7 @@ pub fn main() !void {
     if (pars_info != null) modes_selected += 1;
     if (create_parity_file.len > 0) modes_selected += 1;
     if (watch_mode != null) modes_selected += 1;
-    
+
     if (modes_selected != 1) {
         try print_help();
         return;
@@ -100,7 +123,7 @@ fn print_help() !void {
     print("{s}\n", .{slice_stream.getWritten()});
 }
 
-fn execute_functionality(mode: Mode, check_file: ?[]const u8, pars_info: ?[]const u8, create_parity_file: []const []const u8, watch_mode: ?[]const u8, do_fix: bool, recursive: bool) !void{
+fn execute_functionality(mode: Mode, check_file: ?[]const u8, pars_info: ?[]const u8, create_parity_file: []const []const u8, watch_mode: ?[]const u8, do_fix: bool, recursive: bool) !void {
     switch (mode) {
         .check => {
             if (recursive) {
@@ -124,17 +147,15 @@ fn execute_functionality(mode: Mode, check_file: ?[]const u8, pars_info: ?[]cons
             } else {
                 try do_create_parity_file(data_location, relative_par_location);
             }
-
         },
         .watch => {
             try run_in_watch_mode(watch_mode.?);
-        }
+        },
     }
-
 }
 
-fn recurse_check_files(dir: []const u8, do_fix: bool) !void{
-    var it_dir = std.fs.cwd().openIterableDir(dir, .{}) catch |err|{
+fn recurse_check_files(dir: []const u8, do_fix: bool) !void {
+    var it_dir = std.fs.cwd().openIterableDir(dir, .{}) catch |err| {
         print("{any}", .{err});
         return;
     };
@@ -143,17 +164,15 @@ fn recurse_check_files(dir: []const u8, do_fix: bool) !void{
     var walker = try it_dir.walk(default_allocator);
     defer walker.deinit();
 
-
     var buffer: [4096]u8 = undefined;
     var stringBuilder = strings.StringBuilder.init(buffer[0..]);
-
 
     var dir_separator: u8 = '/';
     if (builtin.os.tag == .windows) {
         dir_separator = '\\';
     }
 
-    while(try walker.next()) |entry| {
+    while (try walker.next()) |entry| {
         stringBuilder.reset();
         stringBuilder.append(dir);
 
@@ -169,66 +188,66 @@ fn recurse_check_files(dir: []const u8, do_fix: bool) !void{
                 std.debug.print("{any}\n", .{err});
             };
         }
-
     }
 }
 
 fn recurse_create_files(dir: []const u8) !void {
-    var it_dir = std.fs.cwd().openIterableDir(dir, .{}) catch |err|{
-            print("{any}", .{err});
-            return;
-        };
-        defer it_dir.close();
+    var it_dir = std.fs.cwd().openIterableDir(dir, .{}) catch |err| {
+        print("{any}", .{err});
+        return;
+    };
+    defer it_dir.close();
 
-        var walker = try it_dir.walk(default_allocator);
-        defer walker.deinit();
+    var walker = try it_dir.walk(default_allocator);
+    defer walker.deinit();
 
+    var buffer: [4096]u8 = undefined;
+    var stringBuilder = strings.StringBuilder.init(buffer[0..]);
 
-        var buffer: [4096]u8 = undefined;
-        var stringBuilder = strings.StringBuilder.init(buffer[0..]);
+    var dir_separator: u8 = '/';
+    if (builtin.os.tag == .windows) {
+        dir_separator = '\\';
+    }
 
+    while (try walker.next()) |entry| {
+        stringBuilder.reset();
+        stringBuilder.append(dir);
 
-        var dir_separator: u8 = '/';
-        if (builtin.os.tag == .windows) {
-            dir_separator = '\\';
+        if (stringBuilder.toSlice()[stringBuilder.toSlice().len - 1] != dir_separator) {
+            stringBuilder.append(&[1]u8{dir_separator});
         }
 
-        while(try walker.next()) |entry| {
-            stringBuilder.reset();
-            stringBuilder.append(dir);
+        stringBuilder.append(entry.path);
 
-            if (stringBuilder.toSlice()[stringBuilder.toSlice().len - 1] != dir_separator) {
-                stringBuilder.append(&[1]u8{dir_separator});
+        const target = stringBuilder.toSlice();
+
+        if (!std.mem.endsWith(u8, target, ".pars")) {
+            const stat = std.fs.cwd().statFile(target) catch {
+                continue;
+            };
+            if (stat.kind != .file) {
+                continue;
             }
-
-            stringBuilder.append(entry.path);
-
-            const target = stringBuilder.toSlice();
-
-            if (!std.mem.endsWith(u8, target, ".pars")) {
-                const stat = std.fs.cwd().statFile(target) catch {
-                    continue;
-                };
-                if (stat.kind != .file) {
-                    continue;
-                }
-                do_create_parity_file(target, null) catch |err| {
-                    //TODO: Handle errors
-                    std.debug.print("{any}\n", .{err});
-                };
-            }
-
+            do_create_parity_file(target, null) catch |err| {
+                //TODO: Handle errors
+                std.debug.print("{any}\n", .{err});
+            };
         }
+    }
 }
 
-fn check_par_file(check_file: []const u8, do_fix: bool) !void{
+fn check_par_file(check_file: []const u8, do_fix: bool) !void {
     const validation_result = ecc.validate_pars_file(check_file, do_fix) catch |err| {
         switch (err) {
-            error.IsDir => {print("File must not be a directory\n", .{});},
-            error.AccessDenied => {print("Access denied\n", .{});},
+            error.IsDir => {
+                print("File must not be a directory\n", .{});
+            },
+            error.AccessDenied => {
+                print("Access denied\n", .{});
+            },
             else => {
                 print("Other error: {any}", .{err});
-            }
+            },
         }
         return;
     };
@@ -239,17 +258,17 @@ fn check_par_file(check_file: []const u8, do_fix: bool) !void{
             print("{s}: file corrupt, size changed, recovery impossible\n", .{check_file});
         } else if (validation_result.unrecoverable_blocks > 0) {
             if (validation_result.recoverable_blocks > 0) {
-                print("{s}: file corrupt, found {d} unrecoverable blocks and {d} recoverable blocks\n", .{check_file, validation_result.unrecoverable_blocks, validation_result.recovered_blocks});
+                print("{s}: file corrupt, found {d} unrecoverable blocks and {d} recoverable blocks\n", .{ check_file, validation_result.unrecoverable_blocks, validation_result.recovered_blocks });
             } else if (validation_result.recovered_blocks > 0) {
-                print("{s}: file corrupt, found {d} unrecoverable blocks and recovered {d} blocks\n", .{check_file, validation_result.unrecoverable_blocks, validation_result.recovered_blocks});
+                print("{s}: file corrupt, found {d} unrecoverable blocks and recovered {d} blocks\n", .{ check_file, validation_result.unrecoverable_blocks, validation_result.recovered_blocks });
             } else {
-                print("{s}: file corrupt, found {d} unrecoverable blocks\n", .{check_file, validation_result.unrecoverable_blocks});
+                print("{s}: file corrupt, found {d} unrecoverable blocks\n", .{ check_file, validation_result.unrecoverable_blocks });
             }
         } else if (validation_result.recoverable_blocks > 0) {
-            print("{s}: file corrupt, recovery possible, found {d} corrupt but recoverable blocks\n", .{check_file, validation_result.recoverable_blocks});
+            print("{s}: file corrupt, recovery possible, found {d} corrupt but recoverable blocks\n", .{ check_file, validation_result.recoverable_blocks });
         } else {
             // If we land here, we must have recovered blocks
-            print("{s}: {d} corrupt blocks found, all recovered", .{check_file, validation_result.recovered_blocks});
+            print("{s}: {d} corrupt blocks found, all recovered", .{ check_file, validation_result.recovered_blocks });
         }
     }
 }
@@ -258,18 +277,28 @@ fn get_par_file_info(pars_info: []const u8) !void {
     const file_header = try ecc.get_pars_file_header(pars_info, default_allocator);
     var blake3_string: [64]u8 = undefined;
     blake_3_as_string(file_header.blake3_hash, &blake3_string);
-    print("File name: {s}\nFile Size: {d}\nBlake 3 hash: {s}\nBlock dimension: {d}\nNumber of full size blocks: {d}\nLast block dimension: {d}\n", .{file_header.file_name, file_header.file_size, blake3_string, file_header.block_dim, file_header.full_size_block_count, file_header.last_block_dim});
+    print("File name: {s}\nFile Size: {d}\nBlake 3 hash: {s}\nBlock dimension: {d}\nNumber of full size blocks: {d}\nLast block dimension: {d}\n", .{ file_header.file_name, file_header.file_size, blake3_string, file_header.block_dim, file_header.full_size_block_count, file_header.last_block_dim });
 }
 
 fn do_create_parity_file(data_file: []const u8, relative_parity_file_location: ?[]const u8) !void {
     try ecc.create_ecc_file_with_datausage(0.01, data_file, relative_parity_file_location);
 }
 
-fn run_in_watch_mode(file: []const u8) ! void {
-    _ = file;
+fn run_in_watch_mode(file: []const u8) !void {
+    var buffer: [4096]u8 = undefined;
+    var file_descriptor = try std.os.inotify_init1(0);
+    var watch_decriptor = try std.os.inotify_add_watch(file_descriptor, file, IN_CLOSE_WRITE);
+    _ = watch_decriptor;
+    const read_size = try std.os.read(file_descriptor, buffer[0..]);
+    _ = read_size;
+    const short_inotify_len = @sizeOf(std.os.linux.inotify_event);
+    var subslice = buffer[0..short_inotify_len];
+    var short_inotify: *align(1) std.os.linux.inotify_event = @ptrCast(subslice);
+    std.debug.print("{any}\n", .{short_inotify.*});
+    std.debug.print("{s}\n", .{buffer[short_inotify_len .. short_inotify_len + short_inotify.len]});
 }
 
-fn blake_3_as_string(bytes: [32]u8, output: *[64]u8) void{
+fn blake_3_as_string(bytes: [32]u8, output: *[64]u8) void {
     const array = "0123456789abcdef";
     var i: usize = 0;
     while (i < output.len) : (i += 2) {
